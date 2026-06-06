8ce99ce64d9bbd15bb2916d3016a61ab5b7696a3
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-06 14:16:16 +0700

    feat(kernel): :sparkles: add index.ts

---
+++ src/kernel/registry/index.ts
@@ -0,0 +1,8 @@
+export { defineFeature } from './feature';
+export type { FeatureModule, NavEntry, IconComponent } from './feature';
+export { createFeatureRegistry } from './feature-registry';
+export type { FeatureRegistry } from './feature-registry';
+export { defineWidget, createWidgetRegistry } from './widget';
+export type { DashboardWidget, WidgetContext } from './widget';
+export { defineGame, createGameRegistry } from './game';
+export type { GameModule, GamePlayProps, GameResult } from './game';
