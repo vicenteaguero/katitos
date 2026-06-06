8964fe160ba86d72e53857f05380ce89000b9f44
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-06 14:16:23 +0700

    feat(kernel): :sparkles: add index.ts

---
+++ src/kernel/supabase/index.ts
@@ -0,0 +1,8 @@
+export { supabase } from './client';
+export type { AppSupabaseClient } from './client';
+export type {
+  Database,
+  Tables,
+  TablesInsert,
+  TablesUpdate,
+} from './database.types';
