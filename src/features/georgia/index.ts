68a1058ea5ac17c1925f7e24047609b8e637482f
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-13 09:25:51 +0700

    feat(georgia): :truck: export countdown widget and queries

--- src/features/georgia/index.ts
+++ src/features/georgia/index.ts
@@ -1,1 +1,3 @@
 export { georgiaFeature } from './feature';
+export { GeorgiaCountdownWidget } from './widgets/georgia-countdown.widget';
+export { useGeorgiaTrip, useGeorgiaPhotos } from './api/georgia.queries';
