b195f1301b4896d38f8816c1c34432b1079a76df
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-13 18:04:53 +0700

    feat(push): :sparkles: export ensure-subscription hook and NotifyResult

--- src/kernel/push/index.ts
+++ src/kernel/push/index.ts
@@ -1,4 +1,7 @@
-export { usePushSubscribe } from './use-push-subscribe';
+export {
+  usePushSubscribe,
+  useEnsurePushSubscription,
+} from './use-push-subscribe';
 export type { PushStatus } from './use-push-subscribe';
 export { notifyPartner } from './notify';
-export type { NotifyPayload } from './notify';
+export type { NotifyPayload, NotifyResult } from './notify';
