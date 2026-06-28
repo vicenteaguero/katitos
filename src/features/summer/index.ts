56ac3d2d80677359b98cc01e78dffb1674c65dad
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-28 17:28:12 +0700

    feat(summer): :sparkles: index.ts

---
+++ src/features/summer/index.ts
@@ -0,0 +1,3 @@
+export { summerFeature } from './feature';
+export { SummerCountdownWidget } from './widgets/summer-countdown.widget';
+export { useSummerTrip, useSummerPhotos } from './api/summer.queries';
