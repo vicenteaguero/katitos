6118d85a5a2ca3c958853ecf3f130ce865eac9b6
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-28 17:27:33 +0700

    feat(album): :sparkles: 3D photo-book index.ts

--- src/features/album/index.ts
+++ src/features/album/index.ts
@@ -1,2 +1,4 @@
 export { albumFeature } from './feature';
 export { AlbumWidget } from './widgets/album-progress.widget';
+export { PhotoBook3D } from './components/photo-book/photo-book-3d';
+export type { PhotoBook3DProps } from './components/photo-book/photo-book-3d';
