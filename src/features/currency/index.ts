de5612424ceb90328d55f16feb72188397c5aa12
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-28 17:27:39 +0700

    feat(currency): :sparkles: index.ts

--- src/features/currency/index.ts
+++ src/features/currency/index.ts
@@ -1,3 +1,4 @@
 export { currencyFeature } from './feature';
 export { CurrencyWidget } from './widgets/currency.widget';
 export { ExchangeIcon } from './components/exchange-icon';
+export { CURRENCIES, type Code } from './currencies';
