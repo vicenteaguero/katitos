079bab0c8dd95fd4a31abc7de7db5255400ab67f
Vicente Agüero <vicenteaguero@uc.cl>
2026-06-28 20:22:29 +0700

    feat(ui): :lipstick: export SectionHeader

--- src/kernel/ui/index.ts
+++ src/kernel/ui/index.ts
@@ -10,6 +10,7 @@ export { Sheet } from './sheet';
 export { Segmented } from './segmented';
 export type { SegmentOption } from './segmented';
 export { PageHeader } from './page-header';
+export { SectionHeader } from './section-header';
 export { Fab } from './fab';
 export { FilePickerButton } from './file-picker-button';
 export type { FilePickerButtonProps } from './file-picker-button';
