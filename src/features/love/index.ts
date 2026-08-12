95a7c70b987f7c4581e03a2482d8286ab74d4472
Vicente Agüero <vicenteaguero@uc.cl>
2026-08-11 22:14:34 -0400

    chore(love): :truck: love feature barrel

---
+++ src/features/love/index.ts
@@ -0,0 +1,4 @@
+export { useLovePhrases } from './api/love-phrases';
+export { PhraseEditor } from './components/phrase-editor';
+export { loveNoteFor, renderPhrase } from './lib/pick-phrase';
+export type { LovePhrase, Gender } from './lib/pick-phrase';
