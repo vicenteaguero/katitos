55bc8330862593001e5365d95a270bb6384c8a5e
Vicente Agüero <vicenteaguero@uc.cl>
2026-08-11 21:46:48 -0400

    chore(polaroid): :truck: export the new day helpers

--- src/features/polaroid/index.ts
+++ src/features/polaroid/index.ts
@@ -1,4 +1,5 @@
 export { polaroidFeature } from './feature';
 export { TodayPolaroidWidget } from './widgets/today-polaroid.widget';
 export { LastPolaroidWidget } from './widgets/last-polaroid.widget';
-export { usePolaroids, useTodayPolaroid } from './api/polaroid.queries';
+export { usePolaroids, useDayPolaroids } from './api/polaroid.queries';
+export { useMyTodayPolaroid } from './api/use-my-today';
