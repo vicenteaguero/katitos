1f6a6fb4a5663f47cc01d67d775579dd00fc8811
Vicente Agüero <vicenteaguero@uc.cl>
2026-08-19 08:56:52 -0400

    feat(build): :sparkles: the build barrel

---
+++ src/kernel/build/index.ts
@@ -0,0 +1,4 @@
+export { BUILD } from './stamp.generated';
+export { BUILD_STATE_LABEL, compareBuilds } from './build-status';
+export type { BuildStamp, BuildState } from './build-status';
+export { useBuildStatus } from './use-build-status';
