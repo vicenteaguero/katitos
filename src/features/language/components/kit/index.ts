03821118058ea2e006593753c1c97694e643c031
Vicente Agüero <vicenteaguero@uc.cl>
2026-08-30 23:09:19 -0400

    feat(language): :sparkles: export the kit

---
+++ src/features/language/components/kit/index.ts
@@ -0,0 +1,5 @@
+export { AudioField } from './audio-field';
+export { VocabRow } from './vocab-row';
+export { ExerciseKindGallery } from './exercise-kind-gallery';
+export { BlockPalette } from './block-palette';
+export { BlockCard } from './block-card';
