083a5c34509f858bdac497eb51e08b60c2ed91d5
Vicente Agüero <vicenteaguero@uc.cl>
2026-08-30 22:01:30 -0400

    refactor(currency): :fire: a home card nobody put on the home screen

--- src/features/currency/index.ts
+++ src/features/currency/index.ts
@@ -1,4 +1,3 @@
 export { currencyFeature } from './feature';
-export { CurrencyWidget } from './widgets/currency.widget';
 export { ExchangeIcon } from './components/exchange-icon';
 export { CURRENCIES, type Code } from './currencies';
