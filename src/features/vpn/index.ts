72e1f336bbc5ab0e3b45b82aa0c2ea08407a4997
Vicente Agüero <vicenteaguero@uc.cl>
2026-09-05 20:23:02 -0400

    refactor(vpn): :fire: stop exporting the widget

--- src/features/vpn/index.ts
+++ src/features/vpn/index.ts
@@ -1,4 +1,3 @@
 export { vpnFeature } from './feature';
-export { TunnelWidget } from './widgets/tunnel.widget';
 export { TunnelButton } from './components/tunnel-button';
 export type { VpnServer, VpnClient } from './types';
