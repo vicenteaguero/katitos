13a989d8f341ecab78cd3b83060805c5d9e5c6cf
Vicente Agüero <vicenteaguero@uc.cl>
2026-09-25 16:45:41 -0300

    refactor(habits): :fire: index belonged to a feature that no longer exists

--- src/features/five/index.ts
+++
@@ -1,4 +0,0 @@
-export { fiveFeature } from './feature';
-export { FiveWidget } from './widgets/five.widget';
-export { FIVE_OPEN } from './lib/goals';
-export { useFiveVisible } from './lib/visible';
